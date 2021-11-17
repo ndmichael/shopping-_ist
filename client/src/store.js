f6c4c4e95b2667febe52af0c37464c2c50218c72
Micheal 'CodeSmith' Ukeje <ukejemicheal@gmail.com>
2021-11-17 02:18:43 +0100

    updated store.js, added new state management

--- client/src/store.js
+++ client/src/store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, compose } from "redux";
+import { composeWithDevTools } from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const middleware = [thunk];
 
-const store = createStore(rootReducer, compose(
+const store = createStore(rootReducer, composeWithDevTools(
     applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
 export default store
